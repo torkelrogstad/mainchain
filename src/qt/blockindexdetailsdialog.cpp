a1ff29ac113d12d62afecf363acbfffca28105fe
cryptaxe <cryptaxe@gmail.com>
2021-05-11 15:29:38 -0700

    Fix merkle tree string bugs, add explainer
    * Fix incorrect hashes being added to merkle tree string
    
    * Add explainer text to merkle tree string

--- src/qt/blockindexdetailsdialog.cpp
+++ src/qt/blockindexdetailsdialog.cpp
@@ -206,6 +206,7 @@ void BlockIndexDetailsDialog::on_pushButtonMerkleTree_clicked()
     dialog.exec();
 }
 
+// TODO move
 // Copy of merkle tree calculator from consensus/merkle.cpp for GUI display
 std::string MerkleTreeString(const std::vector<uint256>& vLeaf, bool& fMutated)
 {
@@ -219,72 +220,58 @@ std::string MerkleTreeString(const std::vector<uint256>& vLeaf, bool& fMutated)
     // Level 0 is the leaves, and the last level is merkle root
     std::vector<std::vector<uint256>> vTree;
 
-    // count is the number of leaves processed so far.
+    // Generate a merkle tree, the same way as consensus/merkle.cpp but
+    // record hashes & leaves to a vector of strings organized into levels of
+    // the tree.
+
     uint32_t count = 0;
-    // inner is an array of eagerly computed subtree hashes, indexed by tree
-    // level (0 being the leaves).
-    // For example, when count is 25 (11001 in binary), inner[4] is the hash of
-    // the first 16 leaves, inner[3] of the next 8 leaves, and inner[0] equal to
-    // the last leaf. The other inner entries are undefined.
     uint256 inner[32];
-    // Which position in inner is a hash that depends on the matching leaf.
     int matchlevel = -1;
-    // Starting position in branch
     uint32_t branchpos = -1;
-    // First process all leaves into 'inner' values.
     while (count < vLeaf.size()) {
         uint256 h = vLeaf[count];
         bool matchh = count == branchpos;
         count++;
         int level;
-        // For each of the lower bits in count that are 0, do 1 step. Each
-        // corresponds to an inner value that existed before processing the
-        // current leaf, and each needs a hash to combine it.
         for (level = 0; !(count & (((uint32_t)1) << level)); level++) {
-            fMutated |= (inner[level] == h);
-            CHash256().Write(inner[level].begin(), 32).Write(h.begin(), 32).Finalize(h.begin());
-
+            // Add a new level to the tree string vector
             if (level >= (int)vTree.size())
                 vTree.push_back(std::vector<uint256>());
 
+            // Record hash for level
             vTree[level].push_back(h);
+
+            fMutated |= (inner[level] == h);
+            CHash256().Write(inner[level].begin(), 32).Write(h.begin(), 32).Finalize(h.begin());
         }
-        // Store the resulting hash at inner position level.
         inner[level] = h;
         if (matchh) {
             matchlevel = level;
         }
+        // Add a new level to the tree string vector
         if (level >= (int)vTree.size())
             vTree.push_back(std::vector<uint256>());
 
+        // Record hash for level
         vTree[level].push_back(h);
     }
-    // Do a final 'sweep' over the rightmost branch of the tree to process
-    // odd levels, and reduce everything to a single top value.
-    // Level is the level (counted from the bottom) up to which we've sweeped.
     int level = 0;
-    // As long as bit number level in count is zero, skip it. It means there
-    // is nothing left at this level.
     while (!(count & (((uint32_t)1) << level))) {
         level++;
     }
     uint256 h = inner[level];
     while (count != (((uint32_t)1) << level)) {
-        // If we reach this point, h is an inner value that is not the top.
-        // We combine it with itself (Bitcoin's special rule for odd levels in
-        // the tree) to produce a higher level one.
         CHash256().Write(h.begin(), 32).Write(h.begin(), 32).Finalize(h.begin());
-        // Increment count to the value it would have if two entries at this
-        // level had existed.
         count += (((uint32_t)1) << level);
         level++;
-        // And propagate the result upwards accordingly.
         while (!(count & (((uint32_t)1) << level))) {
             CHash256().Write(inner[level].begin(), 32).Write(h.begin(), 32).Finalize(h.begin());
 
+            // Add a new level to the tree string vector
             if (level >= (int)vTree.size())
                 vTree.push_back(std::vector<uint256>());
 
+            // Record hash for level
             vTree[level].push_back(h);
 
             level++;
@@ -303,20 +290,60 @@ std::string MerkleTreeString(const std::vector<uint256>& vLeaf, bool& fMutated)
         ss << "Level " << nTreeLevel;
 
         if (nTreeLevel == vTree.size() - 1)
-            ss << " (Root):\n";
+            ss << " (Merkle Root):\n";
         else
         if (nTreeLevel == 0)
-            ss << " (Leaves):\n";
+            ss << " (TxID):\n";
         else
             ss << " :\n";
 
+        // Add hashes with a '|' between every group of 2
+        uint8_t nNode = 0;
         for (const uint256& hash : *ritx) {
+            if (nNode == 2) {
+                nNode = 0;
+                ss << " | ";
+            }
             ss << hash.ToString() << " ";
+            nNode++;
         }
         ss << "\n\n";
 
         nTreeLevel--;
     }
+
+    ss << "-------------------------------\n";
+    ss << "## What is this screen showing?\n";
+
+    ss << "This display allows you to audit the \"hashMerkleRoot\" field. You can see each step of the process yourself.\n\n";
+    ss << "         MerkleRoot = hashZ\n\n";
+    ss << "                hashZ\n";
+    ss << "               /    \\\n";
+    ss << "              /      \\\n";
+    ss << "           hashX       hashY |\n";
+    ss << "           /   \\         \\\n";
+    ss << "          /     \\         \\\n";
+    ss << "         /       \\         \\\n";
+    ss << "     hashF       hashG  |   hashH\n";
+    ss << "    /   \\       /     \\      \\\n";
+    ss << "hashA  hashB | hashC  hashD | hashE\n";
+    ss << "\n\n";
+
+
+    ss << "Notes:\n";
+
+    ss << "1. hashF = Sha256 ( hashA, hashB )\n";
+    ss << "* * Each node in the tree, is the hash of the two nodes beneath it.\n";
+    ss << "2. hashY = Sha256 ( hashH, hashH )\n";
+    ss << "* * In the operations above, Hash \"H\" is repeated on purpose. If there is an odd number of nodes at that level, the final node is hashed with itself.\n";
+    ss << "3. Hashes A through E are TxIDs -- the hashes of each transaction.\n";
+
+    ss << "Use the HashCalculator to check that the Sha256 hash of the first two TxIDs is the value one level above it. And so on ad infinitum.\n";
+
+    ss << "Further Reading:\n";
+    ss << "* https://en.bitcoinwiki.org/wiki/Merkle_tree\n";
+    ss << "* https://www.investopedia.com/terms/m/merkle-tree.asp\n";
+    ss << "* https://www.geeksforgeeks.org/introduction-to-merkle-tree/\n";
 
     return ss.str();
 }
