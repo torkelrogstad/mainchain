d89799f6747cc2678c2bce478fbc8d4710183928
cryptaxe <cryptaxe@gmail.com>
2021-09-09 21:29:53 -0700

    Refactoring, comments & fix custom type bug

--- src/qt/managenewsdialog.cpp
+++ src/qt/managenewsdialog.cpp
@@ -24,20 +24,24 @@ ManageNewsDialog::~ManageNewsDialog()
 
 void ManageNewsDialog::on_pushButtonWrite_clicked()
 {
-    CustomNewsType custom;
-    custom.title = ui->lineEditTitle->text().toStdString();
-
     // Create header bytes
     std::vector<unsigned char> vBytes = ParseHex(ui->lineEditBytes->text().toStdString());
+
+    // Copy header bytes into OP_RETURN script
     CScript script;
     script.resize(vBytes.size() + 1);
     script[0] = OP_RETURN;
-    memcpy(&script[1], &vBytes, vBytes.size());
+    memcpy(&script[1], vBytes.data(), vBytes.size());
 
+    // New custom news type object
+    CustomNewsType custom;
+    custom.title = ui->lineEditTitle->text().toStdString();
     custom.header = script;
 
+    // Save new custom type
     popreturndb->WriteCustomType(custom);
 
+    // Tell widgets we have updated custom types
     Q_EMIT(NewTypeCreated());
     UpdateTypes();
 }
@@ -46,13 +50,19 @@ void ManageNewsDialog::UpdateTypes()
 {
     ui->listWidget->clear();
 
+    // Add the default types to the combo box
     ui->listWidget->addItem("All OP_RETURN data");
     ui->listWidget->addItem("Tokyo Daily News");
     ui->listWidget->addItem("US Daily News");
 
+    // Add the custom types to the combo box
+
     std::vector<CustomNewsType> vCustom;
     popreturndb->GetCustomTypes(vCustom);
 
-    for (const CustomNewsType c : vCustom)
-        ui->listWidget->addItem(QString::fromStdString(c.title));
+    for (const CustomNewsType c : vCustom) {
+        std::string strHex = "(" + HexStr(c.header.begin(), c.header.end()) + ")";
+        QString label = QString::fromStdString(c.title + " | " + strHex);
+        ui->listWidget->addItem(label);
+    }
 }
