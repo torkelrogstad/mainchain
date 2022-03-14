334377d918da0d9b5b2e763acf8261fdc891b030
cryptaxe <cryptaxe@gmail.com>
2022-03-14 15:50:40 -0700

    Update db cache default to 2048 mb & change cache sizes

--- src/init.cpp
+++ src/init.cpp
@@ -1417,12 +1417,10 @@ bool AppInitMain()
     int64_t nTotalCache = (gArgs.GetArg("-dbcache", nDefaultDbCache) << 20);
     nTotalCache = std::max(nTotalCache, nMinDbCache << 20); // total cache cannot be less than nMinDbCache
     nTotalCache = std::min(nTotalCache, nMaxDbCache << 20); // total cache cannot be greater than nMaxDbcache
-    int64_t nBlockTreeDBCache = nTotalCache / 8;
+    int64_t nBlockTreeDBCache = nTotalCache / 4;
     nBlockTreeDBCache = std::min(nBlockTreeDBCache, (gArgs.GetBoolArg("-txindex", DEFAULT_TXINDEX) ? nMaxBlockDBAndTxIndexCache : nMaxBlockDBCache) << 20);
     nTotalCache -= nBlockTreeDBCache;
-    int64_t nSidechainTreeDBCache = nTotalCache / 8;
-    if (nSidechainTreeDBCache > (1 << 21) && !gArgs.GetBoolArg("-txindex", DEFAULT_TXINDEX))
-        nSidechainTreeDBCache = (1 << 21);
+    int64_t nSidechainTreeDBCache = nTotalCache / 4;
     nTotalCache -= nSidechainTreeDBCache;
     int64_t nCoinDBCache = std::min(nTotalCache / 2, (nTotalCache / 4) + (1 << 23)); // use 25%-50% of the remainder for disk cache
     nCoinDBCache = std::min(nCoinDBCache, nMaxCoinsDBCache << 20); // cap total coins db cache
