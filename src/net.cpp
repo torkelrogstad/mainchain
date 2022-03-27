7edc60e3abe0b2b79bf98621066149dbec636ef3
cryptaxe <cryptaxe@gmail.com>
2022-03-27 16:19:09 -0700

    Re-enable 60 second delay before connecting to peers

--- src/net.cpp
+++ src/net.cpp
@@ -1758,13 +1758,8 @@ void CConnman::ThreadOpenConnections(const std::vector<std::string> connect)
         if (interruptNet)
             return;
 
-        // TODO Consider reverting changes for final release after adding DNS
-        // seed nodes.
-        //
-        // Removed 60 second delay before seed nodes are added.
-
         // Add seed nodes if DNS seeds are all down (an infrastructure attack?).
-        if (addrman.size() == 0) {
+        if (addrman.size() == 0 && (GetTime() - nStart > 60)) {
             static bool done = false;
             if (!done) {
                 LogPrintf("Adding fixed seed nodes as DNS doesn't seem to be available.\n");
